fd2b2f21aa045b5fc6309350ea9a392f5bdde79d
Manjistha Swarnakar <167196658+manjistha21@users.noreply.github.com>
2024-07-23 16:52:47 +0530

    Update components-dashboard-sales.tsx

--- components/dashboard/components-dashboard-sales.tsx
+++ components/dashboard/components-dashboard-sales.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import IconSearch from '@/components/icon/icon-search'; // Assuming you have a search icon component
